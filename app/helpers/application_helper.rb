d5b9b3566053beef68444f135f4b726ae1518309
Daniel Penaloza <danielpenalozatj@gmail.com>
2017-07-26 13:24:29 -0700

    Displaying all the posts in the index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	
+	#Return the active word if the current page is the path passed via parameter
+	def active?(path)
+		"active" if current_page?(path)
+	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+puts "One user created"
+
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content")
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "A 100 posts has been created"
