b541c25d01167c0357e8b224d5d898a007c1f9e6
Daniel Penaloza <danielpenalozatj@gmail.com>
2017-07-25 19:05:21 -0700

    Adding the new and create actions for the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
 	def index
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params.require(:post).permit(:date, :rationale))
+		if @post.save
+			flash[:success] = "The post was created successfully."
+			redirect_to @post
+		else
+			flash[:warning] = "There was a problem trying to create this post."
+			render :new
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
 end
