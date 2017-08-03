ba9d30edaabc384308b4adab8dcd38fbb143ba25
Daniel Penaloza <danielpenalozatj@gmail.com>
2017-08-03 15:07:13 -0700

    Added hours requested for post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ puts "One user created"
 AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "User")
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "A 100 posts has been created"
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,19 +1,22 @@
 FactoryGirl.define do
 	factory :post do
-		date 			Date.today
-		rationale 		"Some Rationale"
+		date 				Date.today
+		rationale 			"Some Rationale"
+		overtime_request	3.5
 		user
 	end
 
 	factory :second_post, class: "Post" do
-		date 			Date.yesterday
-		rationale 		"Some More Content"
+		date 				Date.yesterday
+		rationale 			"Some More Content"
+		overtime_request 	1.5
 		user
 	end
 
 	factory :post_from_other_user, class: "Post" do
-		date 			Date.yesterday
-		rationale  		"This post shouldn't be seen"
+		date 				Date.yesterday
+		rationale  			"This post shouldn't be seen"
+		overtime_request 	0.5
 		user {FactoryGirl.create(:non_authorized_user)}
 	end
 end
